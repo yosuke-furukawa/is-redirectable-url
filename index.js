d80d5fa804b254ca40415f063ceede8b7d4e7c62
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2017-08-17 16:54:35 +0900

    fix: return false when path is falsy

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const url = require('url')
 const SAFEHOST = 'https://example.com'
 
 module.exports = function (path) {
+  if (!path) {
+    return false
+  }
   const result = url.parse(url.resolve(SAFEHOST, path))
   return result.href.indexOf(SAFEHOST) >= 0
 }
