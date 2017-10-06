5c8e801f57af8f2225e43574f81376355617d82a
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2017-10-06 10:37:40 -0700

    fix: Check url correctly

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 const url = require('url')
-const SAFEHOST = 'https://example.com'
+const SAFEHOST = 'https://example.com/'
 
 module.exports = function (path) {
   if (!path) {
     return false
   }
   const result = url.parse(url.resolve(SAFEHOST, path))
-  return result.href.indexOf(SAFEHOST) >= 0
+  return result.href.indexOf(SAFEHOST) == 0
 }
